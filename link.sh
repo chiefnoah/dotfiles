1b9482374044252ca454e8bdac6ec4a64c5f8d4d
Joshua Li <josh@jrl.ninja>
2018-09-01 23:11:24 -0400

    tatami4.5: setup manual colorscheme application

--- link.sh
+++ link.sh
@@ -43,7 +43,7 @@ case "$1" in
             arch-og bspwm-og sxhkd-og urxvt-og dunst-og polybar-og pywal-og
 		;;
     rice_tatami4.5)
-		$stow_cmd common common-dev scripts mksh nano micro ranger \
+		$stow_cmd common common-dev bin mksh nano micro ranger mpv cava \
             alpine-tatami4.5 bspwm-tatami4.5 sxhkd-tatami4.5 urxvt-tatami4.5 dunst-tatami4.5
 		;;
 	*)
