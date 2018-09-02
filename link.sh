ef2592e5642ccc1db323387d29d762d67b62cbb7
Joshua Li <josh@jrl.ninja>
2018-09-01 23:09:16 -0400

    unlink functionality

--- link.sh
+++ link.sh
@@ -10,17 +10,25 @@ baseg_dots="atom mpv rofi x"
 arch_dots="arch bspwm sxhkd urxvt dunst compton polybar pywal"
 debian_dots="debian"
 
+if [ "$2" = 'd' ]; then
+    stow_cmd="stow -vDt ${HOME}"
+else
+    stow_cmd="stow -vt ${HOME}"
+fi
+
 case "$1" in
-base)
-	stow -vt "$HOME" $base_dots
-	;;
-arch)
-	stow -vt "$HOME" $base_dots $baseg_dots $arch_dots
-	;;
-debian)
-	stow -vt "$HOME" $base_dots $baseg_dots $debian_dots
-	;;
-*)
-	usage
-	;;
+	base)
+	    $stow_cmd $base_dots
+		;;
+	arch)
+		$stow_cmd $base_dots $baseg_dots $arch_dots
+		;;
+	debian)
+		$stow_cmd $base_dots $baseg_dots $debian_dots
+		;;
+	*)
+		usage
+		;;
 esac
+
+[ ! "$?" -eq 0 ] && printf "You have to move or delete the files stow is conflicting with.\n"
