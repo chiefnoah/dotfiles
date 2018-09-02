9796c2323f3a6608ff3f17bb91cba1ae7d98990d
Joshua Li <josh@jrl.ninja>
2018-09-01 23:09:27 -0400

    better link.sh

--- link.sh
+++ link.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env sh
 
+if ! command -v stow > /dev/null 2>&1; then
+    printf 'You must have GNU stow installed.\b'
+    exit 1
+fi
+
 usage () {
-	printf "USAGE: $0 base|baseg|debian|rice_og\n"
+    cat <<EOF
+USAGE: $0 base|baseg|rice_og [d]
+
+base    - basic configuration for non-graphical environment
+baseg   - basic configuration for graphical environment, inherits base
+rice_og - configuration for Arch Linux rice "og", inherits base, baseg
+
+Pass d as second arg if you want to unlink instead of link.
+EOF
 	exit 1
 }
 
-# dotfile presets
+# configuration presets
 p_base="common common-dev scripts bash zsh nano micro ranger"
 p_baseg="atom mpv rofi redshift"
 p_rice_og="arch-og bspwm-og sxhkd-og urxvt-og dunst-og polybar-og pywal-og"
-
 
 if [ "$2" = 'd' ]; then
     stow_cmd="stow -vDt ${HOME}"
