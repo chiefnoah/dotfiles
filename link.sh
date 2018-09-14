56a8c4391e5242d84e92c8145ca18633034bd792
Joshua Li <josh@jrl.ninja>
2018-09-13 21:41:38 -0700

    split applications based on ng/g, add sqliterc, and more

--- link.sh
+++ link.sh
@@ -10,15 +10,15 @@ USAGE: $0 PRESET [d]
 
 PRESETS:
 base        - all non-graphical, non-dev configs
-baseg       - all X/graphical, non-dev configs
-dev         - blanket development environment config
-devg        - X/graphical dev tools
+baseg       - all graphical, non-dev configs
+dev         - blanket dev environment config
+devg        - graphical dev tools
 og          - config for Arch Linux rice "og"
 tatami4.5   - config for Alpine Linux rice "tatami4.5"
 work        - config for work [currently: yelp]
 
-Pass d as the second argument if you want to unlink the dotfiles belonging
-to the specified macro.
+Pass d as the second argument if you want to unlink the dotfiles
+belonging to the specified macro.
 EOF
 	exit 1
 }
@@ -30,15 +30,15 @@ if
     ! case "$1" in
 	base)
 	    $stow base bin
-        $stow -d applications weechat gnupg
+        $stow -d apps weechat gnupg cava
         $stow -d shells bash mksh zsh
         ;;
     baseg)
-        $stow -d applications cava mpv redshift rofi-pass
+        $stow -d appsg mpv redshift rofi-pass
         ;;
     dev)
         $stow -d dev-langs python golang rust ruby nodejs
-        $stow -d dev-tools basics git nano micro tmux
+        $stow -d dev-tools editorconfig git nano micro tmux sqlite
         $stow -d shells bash mksh zsh
         ;;
     devg)
@@ -46,27 +46,25 @@ if
         ;;
 	og)
 		$stow base bin 'os-arch' 'rice-og'
-        $stow -d applications weechat gnupg \
-            cava mpv redshift rofi-pass
+        $stow -d apps weechat gnupg cava
+        $stow -d appsg redshift rofi-pass
         $stow -d dev-langs python golang rust ruby nodejs
-        $stow -d dev-tools basics git nano micro tmux
+        $stow -d dev-tools editorconfig git nano micro tmux sqlite
         $stow -d shells zsh
         ;;
     tatami4.5)
 		$stow base bin 'os-alpine' 'rice-tatami4.5'
-        $stow -d applications weechat gnupg \
-            mpv
+        $stow -d apps weechat gnupg \
+        $stow -d appsg mpv
         $stow -d dev-langs python golang rust ruby nodejs
-        $stow -d dev-tools basics git nano micro tmux
+        $stow -d dev-tools editorconfig git nano micro tmux
         $stow -d shells mksh
         ;;
     work)
 		$stow --ignore='Brewfile' base bin 'os-mac' 'rice-work'
-        $stow -d applications weechat gnupg \
-            mpv
-        $stow -d dev-langs python
-        $stow -d dev-tools basics nano micro tmux
-        $stow -d shells bash zsh
+        $stow -d appsg mpv
+        $stow -d dev-tools editorconfig micro tmux
+        $stow -d shells zsh
         ;;
 	*)
 		usage ;;
